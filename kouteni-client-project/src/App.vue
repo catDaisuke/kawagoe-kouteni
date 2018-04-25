ccee76e4277640c2a2b628060f456b96949755a0
daisukenakajo <dnakajo.tsukuba@gmail.com>
2018-04-25 16:04:47 +0900

    secondCommit

--- kouteni-client-project/src/App.vue
+++ kouteni-client-project/src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <KouteniHeader></KouteniHeader>
+    <!-- <img src="./assets/logo.png"> -->
     <router-view/>
   </div>
 </template>
 
 <script>
+import KouteniHeader from '@/components/header/KouteniHeader.vue'
+
 export default {
+  components: {
+    KouteniHeader
+  },
   name: 'App'
 }
 </script>
@@ -18,6 +24,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ kouteni-client-project/src/components/page/PageMain.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <!-- <KouteniHeader></KouteniHeader> -->
+</div>
+</template>
+<script>
+import KouteniHeader from '@/components/header/KouteniHeader.vue'
+export default {
+  components: {
+    KouteniHeader
+  },
+  name: 'PageMain',
+  data () {
+    return {
+    }
+  }
+}
+</script>
