b77dfb20464c08a4a4478dc65b2135880ebdd7a1
daisukenakajo <dnakajo.tsukuba@gmail.com>
2018-04-25 16:55:19 +0900

    コンテンツ追加

---
+++ kouteni-client-project/src/components/page/PageMemberList.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h3>メンバー一覧</h3>
+    <b-row>
+        <b-col sm="2">
+        </b-col>
+        <b-col sm="8">
+            <b-list-group>
+                <b-list-group-item v-for="member in memberList" :key="member.id">{{member.name}}</b-list-group-item>
+            </b-list-group>
+        </b-col>
+        <b-col sm="2">
+        </b-col>
+    </b-row>
+</div>
+</template>
+<script>
+import KouteniHeader from '@/components/header/KouteniHeader.vue'
+export default {
+  components: {
+    KouteniHeader
+  },
+  name: 'PageMemberList',
+  data () {
+    return {
+      memberList: [{id: 'simazaki', name: '島崎'}, {id: 'nakajo', name: '中條'}]
+    }
+  }
+}
+</script>
--- kouteni-client-project/src/router/index.js
+++ kouteni-client-project/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import PageMain from '@/components/page/PageMain'
+import PageMemberList from '@/components/page/PageMemberList'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/main',
       name: 'PageMain',
       component: PageMain
+    },
+    {
+      path: '/memberList',
+      name: 'PageMemberList',
+      component: PageMemberList
     }
   ]
 })
