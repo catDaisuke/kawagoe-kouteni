b9ca86572c2ffc906a63dfdd054daaac429ef03e
daisukenakajo <dnakajo.tsukuba@gmail.com>
2018-04-27 13:57:45 +0900

    文言修正

--- src/components/header/KouteniHeader.vue
+++ src/components/header/KouteniHeader.vue
@@ -3,7 +3,7 @@
 
   <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-  <b-navbar-brand href="#">川越高校硬式テニス同好会</b-navbar-brand>
+  <b-navbar-brand href="#">川越高校硬式テニス同好会2006-2009</b-navbar-brand>
 
   <b-collapse is-nav id="nav_collapse">
 
