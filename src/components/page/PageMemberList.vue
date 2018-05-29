50f1df5ddfe3bc3211ec52604adb146dfcf54bf7
daisukenakajo <dnakajo.tsukuba@gmail.com>
2018-05-29 13:19:38 +0900

    メンバレイアウト変更

--- src/components/page/PageMemberList.vue
+++ src/components/page/PageMemberList.vue
@@ -8,16 +8,20 @@
             <!-- <b-list-group>
                 <b-list-group-item v-for="member in memberList" :key="member.memberId">{{member.memberNickName}}</b-list-group-item>
             </b-list-group> -->
-            <b-card-group>
-              <b-card v-for="member in memberList" :key="member.memberId"
-               :title="member.memberNickName"
-               style="max-width: 20rem;"
-              >
-              <p class="card-text">
-                名前：{{member.memberFirstName}} {{member.memberLastName}}<br>
-              </p>
-              </b-card>
-            </b-card-group>
+            <template v-if="vueMemberList != null">
+              <b-row v-for="memberLists in vueMemberList">
+                <b-col v-for="member in memberLists" :key="member.memberId">
+                  <b-card
+                    :title="member.memberNickName"
+                    style="max-width: 20rem;"
+                  >
+                  <p class="card-text">
+                    名前：{{member.memberFirstName}} {{member.memberLastName}}<br>
+                  </p>
+                  </b-card>
+                </b-col>
+              </b-row>
+            </template>
         </b-col>
         <b-col sm="2">
         </b-col>
@@ -35,6 +39,28 @@ export default {
   data () {
     return {
       memberList: null
+    }
+  },
+  computed: {
+    vueMemberList: function () {
+      if (this.$data.memberList == null) {
+        return null
+      }
+      var vueMember = []
+      var list = []
+      console.log(this.$data.memberList)
+      for (var i = 0; i < this.$data.memberList.length; i++) {
+        console.log(list)
+        if (i % 3 === 0 && i > 0) {
+          vueMember.push(list)
+          list = []
+          list.push(this.$data.memberList[i])
+        } else {
+          list.push(this.$data.memberList[i])
+        }
+      }
+      vueMember.push(list)
+      return vueMember
     }
   },
   created: function () {
